75773bf97afd8bdf7d5c37ab779d193b4b71f0da
MuuJian <xairzy@gmail.com>
2025-06-15 20:31:23 +0800

    。

--- cielo.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('jsons/cielo.json', 'r') as f:
-    data = json.load(f)
-
-with open('texts/cielo.txt', 'w', encoding='utf-8') as f:
-    for item in data:
-        f.write(f"{item['wallet']}:{item['label']},\n")
